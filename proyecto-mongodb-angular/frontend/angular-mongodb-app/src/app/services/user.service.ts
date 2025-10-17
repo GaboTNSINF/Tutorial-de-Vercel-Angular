e251d5bc94351b18352a53e5cf189278b08ddc9d
GaboTNSINF <gabrielhernan1261@gmail.com>
2025-10-17 16:08:00 -0300

    initial commit

--- proyecto-mongodb-angular/frontend/angular-mongodb-app/src/app/services/user.service.ts
+++ proyecto-mongodb-angular/frontend/angular-mongodb-app/src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { User, ApiResponse } from '../interfaces/user.interface';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://backend-pal-7.onrender.com';
 
   constructor(private http: HttpClient) { }
 
